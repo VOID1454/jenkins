b08d0fde652db6695f35600bc42858d370edf507
VOID <78741516+VOID1454@users.noreply.github.com>
2022-12-26 16:39:03 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,16 @@
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        
+        int num=3;
+        system.out.println("The a number",num);
+        
+        if (num%2==0.)
+        system.out.println("The entered numbetr is even");
+        else
+        system.out.println("The entered numbetr is odd");
+        
+    }
+}
